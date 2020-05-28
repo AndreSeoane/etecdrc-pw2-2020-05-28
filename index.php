6272670c822b64c89d387db176a5baa851758e5c
André Seoane <AndreSeoane@users.noreply.github.com>
2020-05-28 20:19:54 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
-<?php
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<h1>Hello World</h1>'; ?> 
+
+    <form id="frmlancarnotas" name="frmlancarnotas" action="destino.php" method="POST"> 
+
+    <label>Nome:</label>
+    <input id="txtnome" name="txtnome" type="text" /> 
 
 
-  echo "hello php world" ; 
+    <input id="hdnescondido" name="hdnescondido" type="hidden" value="Vale Alguma Coisa esse Escondidinho!!" /> 
 
 
+    <hr>
+    <button id="btnenviarnotas" name="btnenviarnotas" value="Enviar Notas" > Enviar Notas </button> 
 
-?>
+  </body>
+</html>
